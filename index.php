74e09b0bb240aa0d41e2fadb2683053f353d8482
mrmattmarks <mrmattmarks@gmail.com>
2021-12-28 00:39:38 -0600

    fix horizontal scroll

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
-<html lang="en" >
+<html lang="en">
 <head>
   <meta charset="UTF-8">
   <title>Matt Marks - Product Manager</title>
   <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="style.css?v=2021.12.28.1">
   <link rel="stylesheet" href="flexboxgrid.css">
   <link href="https://fonts.googleapis.com/css?family=Inter:400,500,600&amp;display=swap" rel="stylesheet">
   
-	<script src="https://cdn.usefathom.com/script.js" data-site="PKWQHMDA" defer></script>
+  <script src="https://cdn.usefathom.com/script.js" data-site="PKWQHMDA" defer></script>
 
 </head>
 <body>
@@ -16,62 +16,68 @@
 <header>
   <div id="rainbow"></div>
   <div class="header__bg"></div>
-  <div id="intro">
-	<div class="wrap container-fluid">
+  
+  <section id="intro" class="wrap container-fluid">
 		<div class="row">
-			<div class="col-xs-12 col-md-10 col-md-offset-1">
-				<h1>Hello! I’m Matt Marks, a <br />product manager, <br />web designer, <br />technical ninja, <br />& creative jedi.</h1>
-			</div>
+			<article class="col-xs-12 col-md-10 col-md-offset-1">
+				<h1>Howdy! I’m Matt Marks, a 
+				<br>product manager, 
+				<br>web designer, 
+				<br>technical ninja, 
+				<br>& creative jedi.</h1>
+			</article>
 		</div>
-		<section id="scrollarrow" class="arrow" style="visibility:hidden;">
-		  <a href=""><span></span></a>
-		</section>
-	</div>
-  </div>
+		<div id="scrollarrow" class="arrow" style="visibility:hidden;">
+		  <a href="#productmanager"><span></span></a>
+		</div>
+  </section>
+  
 </header>
 
-<section class="wrap container-fluid">
-	
-	<div class="row">
-		<div class="col-xs-12 col-md-10 col-md-offset-1">
+
+<main>
+
+	<section id="productmanager" class="wrap container-fluid">
+		<div class="row">
+			<div class="col-xs-12 col-md-10 col-md-offset-1">
 		
-			<div class="portfolio-applesupport">
-				<div class="portfolio-applesupport-text fademove">
-					<h3>By day, I work as a Product Manager on the <a href="https://apps.apple.com/us/app/apple-support/id1130498044" target="_blank">Apple Support</a> app.</h3>
-				</div>
-				<div class="fademove iphone13">
-					<div class="fade iphone13-screen"></div>
-				</div>
+				<article class="portfolio-applesupport">
+					<div class="portfolio-applesupport-text fademove">
+						<h2>By day, I work as a Product Manager on the <a href="https://apps.apple.com/us/app/apple-support/id1130498044" target="_blank">Apple Support</a> app.</h2>
+					</div>
+					<div class="fademove iphone13">
+						<div class="fade iphone13-screen"></div>
+					</div>
+				</article>
+		
 			</div>
-		
 		</div>
+	</section>
+
+
+
+	<div class="pixels_bg">
+
+	<section id="pixels" class="wrap container-fluid">
+		<div class="row">
+			<div class="col-xs-12 col-md-10 col-md-offset-1">
+		
+				<article class="portfolio-pixels">
+					<div class="portfolio-pixels-text fademove">
+						<h2>By night, I tickle my imagination with pixels and code.</h2>
+					</div>
+					<div class="fademove pixelsback"></div>
+					<div class="fademove pixelsfront"></div>
+				</article>
+		
+			</div>
+		</div>
+	
+	</section>
+
 	</div>
 
-</section>
-
-
-
-<div class="pixels_bg">
-
-<section class="wrap container-fluid">
-	
-	<div class="row">
-		<div class="col-xs-12 col-md-10 col-md-offset-1">
-		
-			<div class="portfolio-pixels">
-				<div class="portfolio-pixels-text fademove">
-					<h3>By night, I tickle my imagination with pixels and code.</h3>
-				</div>
-				<div class="fademove pixelsback"></div>
-				<div class="fademove pixelsfront"></div>
-			</div>
-		
-		</div>
-	</div>
-	
-</section>
-
-</div>
+</main>
 
 
 
